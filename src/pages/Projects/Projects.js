8a3de81cfbc77fe3603c857db16b930c64498300
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-11-24 20:15:40 -0600

    fixed deploy

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,6 +1,6 @@
 // import "./projects.css";
 
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 const projectList = ["yahtzee", "psb", "posPizza", "weatherApp"];
 
 const Projects = ({ images }) => {
@@ -8,14 +8,6 @@ const Projects = ({ images }) => {
     images;
   const [activeProject, setActiveProject] = useState(null);
   const projectsData = useRef();
-
-  const projectImgs =
-    projectsData.current &&
-    Object.values(projectsData.current).map((p) => p.img);
-
-  const handleClick = (e) => {
-    console.log("e.target", e.target);
-  };
 
   useEffect(() => {
     const data = {
@@ -83,53 +75,18 @@ const Projects = ({ images }) => {
   );
 };
 
-const ProjectCard = ({ details, title, selected, link, ...other }) => {
-  return (
-    <article className={`project-card${selected ? " selected" : ""}`} onClick>
-      <div className="image">
-        <img {...other} alt="project card" width="400" />
-      </div>
-      <div className="info">
-        <p className="title">{title}</p>
-        <p className="details">{details}</p>
-      </div>
-    </article>
-  );
-};
+// const ProjectCard = ({ details, title, selected, link, ...other }) => {
+//   return (
+//     <article className={`project-card${selected ? " selected" : ""}`} onClick>
+//       <div className="image">
+//         <img {...other} alt="project card" width="400" />
+//       </div>
+//       <div className="info">
+//         <p className="title">{title}</p>
+//         <p className="details">{details}</p>
+//       </div>
+//     </article>
+//   );
+// };
 
 export default Projects;
-
-{
-  /* <ProjectCard
-title="Yahtzee"
-details={`
-My fifth and final project for the Flatiron School software engineering bootcamp.
-Combining a React frontend with a Ruby on Rails backend. Written in vanilla CSS and leverages SVGs for game UI
-`}
-src={yahtzeeTn}
-alt="Yahtzee thumbnail"
-selected
-/>
-<ProjectCard
-title="POS-Pizza"
-details={`
-My second project for the Flatiron School software engineering bootcamp.
-This first app I built using React. It exploits JSON Server to mimic a backend.
-(Full MERN update coming soon!) 
-`}
-src={posPizzaTn}
-alt="POS-Pizza thumbnail"
-/>
-<ProjectCard
-title="Weather App"
-details={`Details here`}
-src={weatherAppTn}
-alt="Weather App thumbnail"
-/>
-<ProjectCard
-title="Prentice Street Barbers"
-details={`Details here`}
-src={prenticeStreetBarbersTn}
-alt="Prentice Street Barbers thumbnail"
-/> */
-}
